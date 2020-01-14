6a5bfd702995b392046b7072e78999085c43961b
Albert Jo <for.aljo@gmail.com>
2020-01-13 19:38:23 -0800

    closer

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,6 +5,28 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
-) {
-  // your code here
-};
+  ) {
+    // reference to html, i assume it takes in the elements as well
+    var bodyHtml = document.body;
+
+    //putting results here
+    var result = [];
+
+    //
+    var hasClass = function(bodyHtml) {
+      //if has classes and has this particular classname
+      if (bodyHtml.classList && bodyHtml.classList.contains(className)) {
+        //push that information into result
+        result.push(bodyHtml);
+      }
+      if (bodyHtml.hasChildNodes()) {
+        for (var i = 0; i< bodyHtml.childNodes.length; i++) {
+          hasClass(bodyHtml.childNodes[i]);
+        }
+      }
+    };
+    hasClass(bodyHtml);
+    return result;
+  };
+
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,67 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
 
-// but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
-};
+
+
+  var stringifyJSON = function(values) {
+    // your code goes here
+    var result = []
+    for (var i = 0; i < arguments.length; i++) {
+      if ( arguments[i] === null) {
+        result.push( "null" );
+        if (i < arguments.length - 1) {
+          result.push(stringifyJSON(arguments[i + 1]))
+        }
+        i++
+      }
+      else if (arguments[i] === true) {
+        result.push("true")
+        if (i < arguments.length - 1) {
+         result.push(stringifyJSON(arguments[i + 1]))
+        }
+        i++
+      }
+      else if (arguments[i] === false) {
+        result.push("false")
+        if (i < arguments.length - 1) {
+         result.push(stringifyJSON(arguments[i + 1]))
+        }
+        i++
+      }
+      else if ( typeof arguments[i] === "number") {
+        result.push(arguments[i].toString())
+        if (i < arguments.length - 1) {
+         result.push(stringifyJSON(arguments[i + 1]))
+        }
+        i++
+      }
+
+      else if (typeof arguments[i] === "string") {
+        result.push("\""+ arguments[i] + "\"")
+        if (i < arguments.length - 1) {
+         result.push(stringifyJSON(arguments[i + 1]))
+        }
+        i++
+      }
+      else if (Array.isArray(arguments[i])) {
+        result.push("[" +  stringifyJSON(...arguments[i]) + "]")
+      }
+      else if (typeof arguments[i] === "object") {
+        var temp = []
+        var temp2 = []
+        for (var key in arguments[i]){
+          if (key !== 'functions' && key !== 'undefined') {
+            //console.log(typeof temp, "temp now: " + temp2)
+            temp.push("\"" + key + "\"" + ":" + stringifyJSON(arguments[i][key]))
+            temp = temp.join(',')
+            temp2.push(temp)
+            temp = []
+            //console.log(typeof temp, "temp now: " + temp2)
+            }
+          }
+          temp2 = '{' + temp2 + '}'
+
+        result = result.concat(temp2)
+      }
+    }
+     return result.join(',')
+    };
